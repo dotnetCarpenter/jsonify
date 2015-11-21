c1a06375ba8eb239648f92d453d0aaf5bd684f98
dotnetcarpenter <jon.ronnenberg@gmail.com>
2015-11-21 14:49:29 +0100

    learned streams

--- experiments/Stream.js
+++ experiments/Stream.js
@@ -1,19 +1,23 @@
 "use strict"
 
 function Stream(head, tailPromise) {
-  if (typeof head != 'undefined') {
+  if (typeof head != 'undefined')
     this.headValue = head;
-  }
-  if (typeof tailPromise == 'undefined') {
-    tailPromise = function () {
-      return new Stream();
-    };
-  }
+
+  if (typeof tailPromise == 'undefined')
+    tailPromise = () => new Stream
   this.tailPromise = tailPromise;
 }
 Stream.prototype = {
   empty: function () {
     return typeof this.headValue == 'undefined';
+  },
+  force: function () {
+    // requires finite stream
+    var stream = this;
+    while (!stream.empty()) {
+      stream = stream.tail();
+    }
   },
   head: function () {
     if (this.empty()) {
@@ -21,18 +25,25 @@ Stream.prototype = {
     }
     return this.headValue;
   },
-  tail: function () {
+  item: function (n) {
     if (this.empty()) {
-      throw new Error('Cannot get the tail of the empty stream.');
+      throw new Error('Cannot use item() on an empty stream.');
     }
-    // TODO: memoize here
-    return this.tailPromise();
-  },
-  force: function () {
-    // requires finite stream
-    var stream = this;
-    while (!stream.empty()) {
-      stream = stream.tail();
+    var s = this;
+    while (n != 0) {
+      --n;
+      try {
+        s = s.tail();
+      }
+      catch (e) {
+        throw new Error('Item index does not exist in stream.');
+      }
+    }
+    try {
+      return s.head();
+    }
+    catch (e) {
+      throw new Error('Item index does not exist in stream.');
     }
   },
   map: function (f) {
@@ -61,7 +72,14 @@ Stream.prototype = {
     return new Stream(
       this.head(),
       () => this.tail().take(howmany - 1)
-    )
+      )
+  },
+  tail: function () {
+    if (this.empty()) {
+      throw new Error('Cannot get the tail of the empty stream.');
+    }
+    // TODO: memoize here
+    return this.tailPromise();
   },
   toString: function () {
     // requires finite stream
@@ -86,12 +104,14 @@ var t = new Stream(10, function () {
 // the head of the t stream is 10; its tail has a head which is 20 and a tail which  
 // has a head which is 30 and a tail which is the empty stream.  
 //t.print(); // prints 10, 20, 30
-//console.log(t)
-test("abc")
+//console.log( t )
+let testStream = test("abc") 
+console.log(testStream)
+console.log(testStream.item(2))
 
-function test(list, acc = []) {
-  if(list.length === 0) return acc
+function test(list) {
+  if (list.length === 0) return new Stream
   let fst = list[0],
-      rest =  list.substr(list.length-1)
-  return new Stream(fst, test(rest))
+    rest = list.substr(fst.length)
+  return new Stream(fst, () => test(rest))
 }
